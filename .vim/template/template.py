0364643bb6bbe023244ee786b4c396f3952b70d1
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-06-19 02:08:16 +0900

    ADDED .vim to the tracking files

---
+++ .vim/template/template.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+#-*- coding: utf-8 -*-
+#
+# Author:   kentarowada
+# Mail:     www.kentaro.wada@gmail.com
+# URL:      http://wkentaro.com
+# Created:  <+DATE+>
+# Filename: <+FILENAME+>
+#
+<+CURSOR+>
