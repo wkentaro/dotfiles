38ab0af3ac7f02252c866b2b2b4cea853c03917f
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-11-13 02:11:25 +0900

    Remove unnecessary emacs.d

--- .emacs.d/config/01-auto-complete.el
+++
@@ -1,5 +0,0 @@
-(when (require 'auto-complete-config nil t)
-  (add-to-list 'ac-dictionary-directories
-               "~/.emacs.d/auto-complete")
-  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-  (ac-config-default))
