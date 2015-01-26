a781ed7e7a8251ab6cdffe9573d60c01e57b2d08
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-01-26 23:40:31 +0900

    update template.py

--- vim/template/template.py
+++ vim/template/template.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-
-__author__ = "www.kentaro.wada@gmail.com (Kentaro Wada)"
-
 from __future__ import print_function, division, unicode_literals
 <+HERE+>
