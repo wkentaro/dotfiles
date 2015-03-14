984b1ff905e83c720e33f8e5380cd9066f10b8d1
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-03-15 06:32:58 +0900

    emacs.d/init.el: simplify emacs configuration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -24,27 +24,14 @@
 (setq grep-command (cons (concat grep-command-before-query " .")
                          (+ (length grep-command-before-query) 1)))
 
-
-;;; メニューバーを消す
-(menu-bar-mode -1)
-
-;;; ツールバーを消す
-(tool-bar-mode -1)
-
-;;; カーソルの点滅をとめる
-(blink-cursor-mode 0)
+(define-key global-map "\C-h" 'delete-backward-char) ; delete
+(define-key global-map "\M-?" 'help-for-help) ; help
 
 ;;; 対応する括弧を光らせる
 (show-paren-mode 1)
 
 ;;; ウィンドウ内に収まらないときだけカッコ内も光らせる
 (setq show-paren-style 'mixed)
-
-;;; 行末の空白を表示　
-;(setq-default show-trailing-whitespace t)
-
-;;; 現在行を目立たせる　
-; (global-hl-line-mode)
 
 ;;; 行の先頭をC-kを一回押すだけで行全体を表示する
 (setq kill-whole-line t)
@@ -61,10 +48,6 @@
 ;;; 終了時にオートセーブファイルを消す
 (setq delete-auto-save-files t)
 
-
-
-
-
 ;;; 補完
 (setq completion-ignore-case t)
 (setq read-file-name-completion-ignore-case t)
@@ -76,8 +59,6 @@
 ;; 補完可能なものを随時表示
 (icomplete-mode 1)
 
-
-
 ;;; 履歴数
 (setq history-length 10000)
 
@@ -87,32 +68,22 @@
 ;;; 最近開いたファイルを保存する数
 (setq recentf-max-saved-items 10000)
 
-
-
 ;;; ediffを1ウィンドウで実行
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
 ;;; diffのオプション
 (setq diff-switches '("-u" "-p" "-N"))
 
-
-
-
-
-
 ;;; diredを便利にする
 (require 'dired-x)
-
 
 ;;; diredから"r"でファイル名インライン編集する
 (require 'wdired)
 (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
 
-
 ;;; ファイル名が重複していたらディレクトリ名を追加する
 (require 'uniquify)
 (setq uniqufy-buffer-name-style 'post-forward-angle-brackets)
-
 
 ;;; デフォルトのタブ
 (setq-default indent-tabs-mode nil)
@@ -143,3 +114,4 @@
 ;   (require 'rosemacs)
 ;   (invoke-rosemacs)
 ;   (global-set-key "\C-x\C-r" ros-keymap))
+
