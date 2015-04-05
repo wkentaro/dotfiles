0e178f63d1c88668bcce2a1542227aadf7c12765
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-04-05 18:23:38 +0900

    [link_dotfiles.py] Update for ignore files

--- link_dotfiles.py
+++ link_dotfiles.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# link_dotfiles.py
-# author: Kentaro Wada <www.kentaro.wada@gmail.com>
+#
 
 import os
 
@@ -9,16 +8,16 @@ home = os.path.expanduser('~')
 dotfiles_dir = os.path.join(home, '.dotfiles')
 files = os.listdir(dotfiles_dir)
 
+link_ignores = ['LICENSE.txt',
+                'install_vim.sh',
+                'iterm2-memo.txt',
+                'link_dotfiles.py']
+
 for f in files:
     if f.startswith('.'):
-        continue  # .git and .gitignore is passed
-    elif f.endswith('.txt'):
-        continue
-    elif f.endswith('.sh'):
+        # ignore .git or .gitmodules
         continue
     file1 = os.path.join(dotfiles_dir, f)
     file2 = os.path.join(home, '.'+f)
-    if os.access(file1, os.X_OK):
-        continue  # executable script is passed
     print('ln -s {0} {1}'.format(file1, file2))
     os.system('ln -s {0} {1}'.format(file1, file2))
