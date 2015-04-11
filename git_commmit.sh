3c86f96ea7a1dd9e63a11ac9f0e3cf60f94a3b2a
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-04-12 05:42:21 +0900

    [git_commmit.sh] add prompt to publish or not

--- git_commmit.sh
+++ git_commmit.sh
@@ -7,3 +7,12 @@ for file in ${changed_files}; do
   echo "[${file}]" > /tmp/git_commit_message_template
   git commit --verbose --template /tmp/git_commit_message_template
 done
+
+current_branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+read -p "push to origin? [y/n]: " yn
+case $yn in
+  [Yy]* ) git push origin ${current_branch};;
+  [Nn]* ) exit;;
+  * ) echo "Please answer yes or no.";;
+esac
+
