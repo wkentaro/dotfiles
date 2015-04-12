9f71a0d37ebef33fb7cdfff7a3ee6b4ec6cf530b
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-04-13 05:08:35 +0900

    [git_commit.sh] Update for push or not

--- git_commmit.sh
+++ git_commmit.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 #
-
-changed_files=`git status -s | sed 's/M  //g' | sed 's/ M //g' | sed 's/\?\? //g'`
+push=0
+git add --all
+changed_files=`git status -s | sed 's/^...//g' | sed 's/ -> /,/g'`
 for file in ${changed_files}; do
-  git add ${file}
-  echo "[${file}]" > /tmp/git_commit_message_template
-  git commit --verbose --template /tmp/git_commit_message_template
+  msg=`echo ${file} | sed "s/.*,//g"`
+  echo "[${msg}]" > /tmp/git_commit_message_template
+  files=`echo ${file} | tr ',' ' '`
+  git commit --only ${files} --verbose --template /tmp/git_commit_message_template || break
+  push=1
 done
 
-current_branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
-read -p "push to origin? [y/n]: " yn
-case $yn in
-  [Yy]* ) git push origin ${current_branch};;
-  [Nn]* ) exit;;
-  * ) echo "Please answer yes or no.";;
-esac
+if [ $push = 1 ]; then
+  current_branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+  read -p "push to origin? [y/n]: " yn
+  case $yn in
+    [Yy]* ) git push origin ${current_branch};;
+    [Nn]* ) exit;;
+    * ) echo "Please answer yes or no.";;
+  esac
+fi
 
