f314d91120449fea18392832fed2128aa4a8cdcd
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-05-17 17:07:48 +0900

    update install_tmux

--- scripts/install_tmux_linux.sh
+++ scripts/install_tmux_linux.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 #
+if which atool >/dev/null 2>&1; then
+    sudo aptitude install atool
+fi
+if $(dpkg --get-selections | grep libevent-dev | wc -l) != 1; then
+    sudo aptitude install libevent-dev
+fi
 wget http://sourceforge.net/projects/tmux/files/tmux/tmux-1.9/tmux-1.9a.tar.gz
 atool -x tmux-1.9a.tar.gz
-cd tmu-1.9a
+cd tmux-1.9a
 ./configure && make && sudo make install
