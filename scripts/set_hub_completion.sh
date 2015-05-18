ee62166f1e867647914213c981c80f92ec87fe55
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-05-18 15:49:27 +0900

    fix _hub path

--- scripts/set_hub_completion.sh
+++ scripts/set_hub_completion.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
 
-sudo wget https://github.com/github/hub/blob/master/etc/hub.zsh_completion -O /usr/local/share/zsh/site-functions/_hub
+sudo wget https://raw.githubusercontent.com/github/hub/master/etc/hub.zsh_completion -O /usr/local/share/zsh/site-functions/_hub
