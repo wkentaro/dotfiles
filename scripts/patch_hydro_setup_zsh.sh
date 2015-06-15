93a225c93334f479f767d5539e6e7286b1d6076c
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-06-16 02:15:34 +0900

    [scripts/patch_hydro_setup_zsh.sh] update

--- scripts/patch_hydro_setup_zsh.sh
+++ scripts/patch_hydro_setup_zsh.sh
@@ -1,10 +1,12 @@
-echo \
-"""6,8c6
+echo """6,8c6,9
 < emulate sh # emulate POSIX
 < . "$_CATKIN_SETUP_DIR/setup.sh"
 < emulate zsh # back to zsh mode
 ---
+> # emulate sh # emulate POSIX
+> # . "$_CATKIN_SETUP_DIR/setup.sh"
+> # emulate zsh # back to zsh mode
 > emulate -R zsh -c 'source "$_CATKIN_SETUP_DIR/setup.sh"'""" \
-  > /tmp/hydro_setup_zsh.diff
+> /tmp/hydro_setup_zsh.diff
 
-sudo patch /opt/ros/hydro/setup.zsh < /tmp/hydro_setup_zsh.diff
+sudo patch $1 < /tmp/hydro_setup_zsh.diff
