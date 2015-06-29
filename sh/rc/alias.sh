fd39442058451140adf249f78097b57bff3539b4
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-06-30 03:50:40 +0900

    [sh/rc/alias.sh] Cleanup aliases

--- sh/rc/alias.sh
+++ sh/rc/alias.sh
@@ -1,49 +1,35 @@
 # vim: set ft=sh:
 
-alias v='vim'
-alias vi='vim'
+# ----------------------------------------------------
+# Basics
+# ----------------------------------------------------
 alias c='clear'
+alias lv='less'
+alias vi='vim'
 alias py='python'
 alias ipy='ipython'
-alias sl='ls'
-alias ll='ls -lha'
 alias emacs='emacs -nw'
-alias ga.='git add .'
-alias gaa='git add --all'
-alias gcln='git clone'
-alias grm='git rm'
-alias gmv='git mv'
-alias ggpush!='git push origin $(current_branch) --force'
-alias gmpush='git push $GITHUB_USER $(current_branch)'
-alias gmpush!='git push $GITHUB_USER $(current_branch) --force'
-alias grb='git rebase'
-alias grbg='git rebase origin/master'
-alias gmpull='git pull $GITHUB_USER $(current_branch)'
-alias gmpnp='git pull $GITHUB_USER $(current_branch) && git push $GITHUB_USER $(current_branch)'
-alias gcal='open https://www.google.com/calendar/render#g'
+
+# ----------------------------------------------------
+# browser
+# ----------------------------------------------------
+alias gcal='open https://www.google.com/calendar/render#g >/dev/null 2>&1'
 alias gmail='open https://mail.google.com/mail/u/0/ >/dev/null 2>&1'
+alias github='open https://github.com >/dev/null 2>&1'
+
+# ----------------------------------------------------
+# tmux aliases
+# ----------------------------------------------------
 alias t='tmux'
 alias tls='tmux ls'
 alias ta='tmux attach'
 alias tat='tmux attach -t'
 alias tn='tmux new'
 alias tns='tmux new -s'
-alias gpr='hub pull-request'
-alias gpl='hub browse -- pulls >/dev/null 2>&1'
-alias gis='hub browse -- issues >/dev/null 2>&1'
-alias gbw='git browse $@ >/dev/null 2>&1'
-alias gbd='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
-alias gbdr='git branch -r --merged origin/master | grep "$GITHUB_USER\\/" | sed "s/$GITHUB_USER\\///" | egrep -v "HEAD|master|develop|release" | xargs git push $GITHUB_USER --delete'
-alias grbg='git rebase origin/master'
-alias grbgi='git rebase -i origin/master'
-alias gbD='git branch -D'
-alias gf='git fetch'
-alias gfa='git fetch --all'
-alias gfap='git fetch --all --prune'
-alias lv='less'
-alias gremote2local='cbranch=$(current_branch) ; git branch --all | grep $GITHUB_USER | egrep -v "HEAD|master|develop|release" | sed "s/^  remotes\/$GITHUB_USER\///" | xargs -n1 -I{} git branch {} --track $GITHUB_USER/{} ; git checkout $cbranch'
 
+# ----------------------------------------------------
 # Use rlwrap commands
+# ----------------------------------------------------
 if which rlwrap >/dev/null 2>&1; then
     alias eus='rlwrap eus'
     alias irteusgl='rlwrap irteusgl'
@@ -52,22 +38,31 @@ if which rlwrap >/dev/null 2>&1; then
     alias clisp="rlwrap -b '(){}[],#\";| ' clisp"
 fi
 
-# Use hub as git client
-if which hub >/dev/null 2>&1; then
-  eval "$(hub alias -s)"
+# ----------------------------------------------------
+# ros
+# ----------------------------------------------------
+if [ -d "/opt/ros" ]; then
+    alias rqt_gui='rosrun rqt_gui rqt_gui'
+    alias rqt_reconfigure='rosrun rqt_reconfigure rqt_reconfigure'
+    alias image_view=`rosrun image_view image_view image:=$1`
 fi
 
-# Improve open
+# ----------------------------------------------------
+# open aliases
+# ----------------------------------------------------
 if which gnome-open >/dev/null 2>&1; then  # linux
     alias open='gnome-open'
     alias o='gnome-open'
     alias o.='gnome-open .'
-elif which open >/dev/null 2>&1; then  # mac
+elif which open >/dev/null 2>&1; then  # osx
     alias o='open'
     alias o.='open .'
 fi
 
-# Improve ls
+# ----------------------------------------------------
+# ls aliases
+# ----------------------------------------------------
+alias sl='ls'
 if [ $TERM = "dumb" ]; then
     # Disable colors in GVim
     alias ls="ls -F --show-control-chars"
@@ -85,7 +80,38 @@ else
     fi
 fi
 
-# Git commit each file
+# ----------------------------------------------------
+# git aliases
+# ----------------------------------------------------
+# Use hub as git client
+[ which hub >/dev/null 2>&1 ] && eval "`hub alias -s`"
+# command aliases
+alias ga.='git add .'
+alias gaa='git add --all'
+alias gcln='git clone'
+alias grm='git rm'
+alias gmv='git mv'
+alias ggpush!='git push origin $(current_branch) --force'
+alias gmpush='git push $GITHUB_USER $(current_branch)'
+alias gmpush!='git push $GITHUB_USER $(current_branch) --force'
+alias grb='git rebase'
+alias grbg='git rebase origin/master'
+alias gmpull='git pull $GITHUB_USER $(current_branch)'
+alias gmpnp='git pull $GITHUB_USER $(current_branch) && git push $GITHUB_USER $(current_branch)'
+alias gpr='hub pull-request'
+alias gpl='hub browse -- pulls >/dev/null 2>&1'
+alias gis='hub browse -- issues >/dev/null 2>&1'
+alias gbw='git browse $@ >/dev/null 2>&1'
+alias gbd='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+alias gbdr='git branch -r --merged origin/master | grep "$GITHUB_USER\\/" | sed "s/$GITHUB_USER\\///" | egrep -v "HEAD|master|develop|release" | xargs git push $GITHUB_USER --delete'
+alias grbg='git rebase origin/master'
+alias grbgi='git rebase -i origin/master'
+alias gbD='git branch -D'
+alias gf='git fetch'
+alias gfa='git fetch --all'
+alias gfap='git fetch --all --prune'
+alias gremote2local='cbranch=$(current_branch) ; git branch --all | grep $GITHUB_USER | egrep -v "HEAD|master|develop|release" | sed "s/^  remotes\/$GITHUB_USER\///" | xargs -n1 -I{} git branch {} --track $GITHUB_USER/{} ; git checkout $cbranch'
+# commit each file
 _git_commit_each_file () {
     changed_files=`git status -s | grep "^[A-Z]" | sed 's/^...//g' | sed 's/ -> /,/g'`
     changed_files=(`echo $changed_files`)
