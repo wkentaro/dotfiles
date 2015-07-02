24fd32250efb80355ff230af45fab2752faa7082
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-03 03:24:20 +0900

    latex installer

---
+++ scripts/install_latex_trusty.sh
@@ -0,0 +1,6 @@
+sudo apt-get -y install texlive
+sudo apt-get -y install texlive-lang-cjk
+sudo apt-get -y install xdvik-ja
+sudo apt-get -y install dvipsk-ja
+sudo apt-get -y install gv
+sudo apt-get -y install texlive-fonts-recommended texlive-fonts-extra
