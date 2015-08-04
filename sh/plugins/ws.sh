b905828e9703d36522820d88c01ddf406e57bdf2
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-08-05 05:48:27 +0900

    clenaup sh plugins -> zsh/plugins or sh/plugins

---
+++ sh/plugins/ws.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+
+ws () {
+  local ws
+  ws=$(cat ~/.ws_list | xargs -n1 | percol)
+  [ "$ws" = "" ] && return 1
+  ws=$(echo $ws | sed "s,^~,$HOME,")
+  cd $ws
+}
+
+register_ws () {
+  echo $(pwd | sed "s,^$HOME,~,") >> ~/.ws_list
+}
+
+unregister_ws () {
+  local ws
+  ws=$(cat ~/.ws_list | xargs -n1 | percol)
+  [ "$ws" = "" ] && return 1
+  out=$(echo $ws | sed 's,/,\\/,g' | sed 's,^\~,\\~,')
+  sed -i -e "/$out/d" ~/.ws_list
+  echo "$ws is removed"
+}
