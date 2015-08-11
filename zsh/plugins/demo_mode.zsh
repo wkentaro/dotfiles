becd13316e2543ccf93740b942b2afd8602d7f4f
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-08-12 05:05:35 +0900

    Add bash option for demo_mode

--- zsh/plugins/demo_mode.zsh
+++ zsh/plugins/demo_mode.zsh
@@ -1,8 +1,20 @@
 #!/usr/bin/env zsh
 
 demo_mode () {
+  local shl prpt
+  shl="$1"
+  if [ "$shl" = "" ]; then
+    shl="zsh"
+  fi
+  case "$shl" in
+    (bash)
+      prpt='$ ' ;;
+    (zsh)
+      prpt='%# ' ;;
+    *) ;;
+  esac
   _OLD_PROMPT=$PROMPT
-  PROMPT='%# '
+  PROMPT=$prpt
 }
 
 exit_demo_mode() {
