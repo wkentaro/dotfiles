18eaa4e3687835f83a1ae7a818d1c79b41b1018e
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-08-25 00:32:49 +0900

    Use el-get for emacs package management

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,59 +1,39 @@
-(setq load-path (append '("~/.emacs.d/packages") load-path))
+;; package
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
+(el-get-bundle auto-complete)
+(el-get-bundle evil)
+(el-get-bundle kawabata/emacs-trr)
+
+;; for Homebrew
+(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+;; basic
 (setq-default indent-tabs-mode nil)
-
 (define-key global-map "\C-h" 'delete-backward-char) ; delete
 (define-key global-map "\M-?" 'help-for-help) ; help
-
 (show-paren-mode 1)
-
 (setq show-paren-style 'mixed)
-
 (setq kill-whole-line t)
-
 (setq require-final-newline t)
-
 (setq next-line-add-newlines nil)
-
 (setq make-backup-files nil)
-
 (setq delete-auto-save-files t)
-
-;; avoid "Symbolic link to SVN-controlled source file; follow link? (yes or no)"
-(setq vc-follow-symlinks t)
-
 (setq completion-ignore-case t)
 (setq read-file-name-completion-ignore-case t)
-
 (icomplete-mode 1)
-
 (setq history-length 10000)
-
 (savehist-mode 1)
-
 (setq recentf-max-saved-items 10000)
-
 (setq-default indent-tabs-mode t)
 (setq-default tab-width 4)
 
-;;; auto-install
-(require 'auto-install)
-(setq auto-install-directory "~/.emacs.d/packages/")
-(add-to-list 'load-path auto-install-directory)
-; (auto-install-update-emacswiki-package-name t)
-(auto-install-compatibility-setup)
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
-
-;; trr
-(add-to-list 'load-path "~/.emacs.d/packages/platform-p")
-(require 'platform-p)
-(setq TRR:japanese nil)
-(when platform-darwin-p
-  (let ((default-directory "/usr/local/share/emacs/site-lisp/"))
-    (normal-top-level-add-subdirs-to-load-path))
-  (require 'trr)
-  )
-(when platform-linux-p
-  (add-to-list 'load-path "/usr/share/emacs/site-lisp/trr22")
-  (add-to-list 'load-path "/usr/local/share/emacs/24.3/site-lisp/emu")
-  (autoload 'trr "/usr/share/emacs/site-lisp/trr22/trr" nil t))
+;; avoid yn for symlink
+(setq vc-follow-symlinks t)
