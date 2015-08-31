7167333c69e63160b9bd1f01635e782a40d2a2be
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-09-01 04:30:25 +0900

    Add antibody

---
+++ zsh/antibody/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+BASE_URL="https://github.com/caarlos0/antibody/releases/download"
+VERSION="v0.2.2"
+ARCH="386"
+OS="$(uname -s | tr "[:upper:]" "[:lower:]")"
+if [ "$(uname -m)" = "x86_64" ]; then
+  ARCH="amd64"
+fi
+mkdir -p ~/.dotfiles/zsh/antibody/antibody
+wget -O /tmp/antibody.tar.gz \
+  "${BASE_URL}/${VERSION}/antibody_${OS}_${ARCH}.tar.gz"
+tar xvzf /tmp/antibody.tar.gz -C ~/.dotfiles/zsh/antibody/antibody
