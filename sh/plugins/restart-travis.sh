09d247ac59463b1ae0479348074a6b262f3ce671
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-09-02 23:16:37 +0900

    restart also errored travis test

--- sh/plugins/restart-travis.sh
+++ sh/plugins/restart-travis.sh
@@ -7,12 +7,8 @@ _current_github_repo () {
 }
 
 restart_failed_travis_on_pr_branch () {
-  type travis &>/dev/null || {
-    echo -e "Please run:\n$ sudo gem install travis -v 1.8.0 --no-rdoc --no-ri"
-    return 1
-  }
   github_repo=`_current_github_repo`
-  failed_job_ids=(`travis show | grep '^#.* failed:' | sed 's/^#//' | awk '{print $1}'`)
+  failed_job_ids=(`travis show $@ | grep '^#.* \(failed\|errored\):' | sed 's/^#//' | awk '{print $1}'`)
   for job_id in $failed_job_ids; do
     echo "\"restart travis $github_repo $job_id\" -> #travis at jsk-robotics.slack.com"
     echo "restart travis $github_repo $job_id" | slacker -c travis
