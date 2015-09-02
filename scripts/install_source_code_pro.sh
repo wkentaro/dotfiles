903cdfc63c17f878b79a4cb8c31ba011516c2d3e
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-09-02 13:44:43 +0900

    Add script

---
+++ scripts/install_source_code_pro.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir /tmp/adodefont
+cd /tmp/adodefont
+wget https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip
+unzip 1.030R-it.zip
+mkdir -p ~/.fonts
+cp source-code-pro-2.010R-ro-1.030R-it/OTF/*.otf ~/.fonts/
+fc-cache -f -v
