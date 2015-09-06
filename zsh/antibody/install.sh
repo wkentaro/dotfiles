335b52f68998012250430364fc24543953c86f82
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-09-06 17:54:56 +0900

    antibody v0.3.4

--- zsh/antibody/install.sh
+++ zsh/antibody/install.sh
@@ -5,7 +5,7 @@ if [ -f ~/.dotfiles/zsh/antibody/antibody/bin/antibody ]; then
 fi
 
 BASE_URL="https://github.com/caarlos0/antibody/releases/download"
-VERSION="v0.2.3"
+VERSION="v0.3.4"
 ARCH="386"
 OS="$(uname -s | tr "[:upper:]" "[:lower:]")"
 if [ "$(uname -m)" = "x86_64" ]; then
