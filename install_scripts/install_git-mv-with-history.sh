a0f9c993d6fc2dd8a0d3b8413303f7ca62769ab6
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-09-28 18:34:43 +0900

    Add install_git-mv-with-history

---
+++ install_scripts/install_git-mv-with-history.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# git-mv-with-history
+# ~~~~~~~~~~~~~~~~~~~
+
+if type git-mv-with-history &>/dev/null; then
+  exit 0
+fi
+
+mkdir -p $HOME/.local/bin
+wget https://gist.githubusercontent.com/emiller/6769886/raw/ae47266e867438b9cbd188fb6851ca6566e241d0/git-mv-with-history -O $HOME/.local/bin/git-mv-with-history --no-check-certificate
+chmod u+x $HOME/.local/bin/git-mv-with-history
