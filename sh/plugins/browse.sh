20a450a321e5bc4dba43cb34a4c6465fa8a973c2
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-10-24 19:44:30 +0900

    fix browse

--- sh/plugins/browse.sh
+++ sh/plugins/browse.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 browse () {
-  local url
+  local url os
   [ "$1" = "" ] && return 1
   url=$(python -c "\
 import re
@@ -11,6 +11,11 @@ if m is None:
   url = 'http://' + url
 print(url)
 " 2>&1)
-  open $url
+  os=$(uname)
+  if [ "$os" = "Linux" ]; then
+    gnome-open $url 1>/dev/null 
+  elif [ "$os" = "Darwin" ]; then
+    open $url 1>/dev/null
+  fi
 }
 alias b='browse'
