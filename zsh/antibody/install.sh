d67d6c431458b9da966661a9032d6761d9a83d46
Takasugi Noriaki <takasugi@jsk.imi.i.u-tokyo.ac.jp>
2015-11-03 21:44:46 +0900

    home zsh

--- zsh/antibody/install.sh
+++ zsh/antibody/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -f ~/.dotfiles/zsh/antibody/antibody/bin/antibody ]; then
+if [ -f ~/.zsh/antibody/antibody/bin/antibody ]; then
   exit 0
 fi
 
@@ -11,7 +11,7 @@ OS="$(uname -s | tr "[:upper:]" "[:lower:]")"
 if [ "$(uname -m)" = "x86_64" ]; then
   ARCH="amd64"
 fi
-mkdir -p ~/.dotfiles/zsh/antibody/antibody
+mkdir -p ~/.zsh/antibody/antibody
 wget -O /tmp/antibody.tar.gz \
   "${BASE_URL}/${VERSION}/antibody_${OS}_${ARCH}.tar.gz"
-tar xvzf /tmp/antibody.tar.gz -C ~/.dotfiles/zsh/antibody/antibody
+tar xvzf /tmp/antibody.tar.gz -C ~/.zsh/antibody/antibody
