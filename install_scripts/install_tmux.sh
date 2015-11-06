ee3797f632d919dd6aa6f5e5f77967cfc12ed183
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-06 14:21:01 +0900

    install_tmux udpate

--- install_scripts/install_tmux.sh
+++ install_scripts/install_tmux.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ -f $HOME/.local/bin/tmux ]; then
+  exit 0
+fi
+
+sudo aptitude install libevent-dev
+
 wget http://sourceforge.net/projects/tmux/files/tmux/tmux-1.9/tmux-1.9a.tar.gz
 tar zxvf tmux-1.9a.tar.gz
 cd tmux-1.9a
