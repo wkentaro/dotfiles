86bda8e2042b3f03265a31d1d659a9010d2bad34
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-15 03:41:42 +0900

    Add install_ruby.sh

---
+++ install_scripts/install_ruby.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+cd
+wget http://ftp.ruby-lang.org/pub/ruby/2.2/ruby-2.2.3.tar.gz
+tar -xzvf ruby-2.2.3.tar.gz
+cd ruby-2.2.3/
+./configure
+make
+sudo make install
+ruby -v
+rm -r ruby-2.2.3
+rm -r ruby-2.2.3.tar.gz
