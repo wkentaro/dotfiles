58bd0fff6461adebeedf0e05b7056d5d8aa85d76
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-23 03:26:00 +0900

    Check if installed in install_ubuntu_tools.sh

--- install_scripts/install_ubuntu_tools.sh
+++ install_scripts/install_ubuntu_tools.sh
@@ -4,25 +4,29 @@ if [ "$(uname)" != "Linux" ]; then
   exit 1
 fi
 
-sudo apt-get install aptitude -y
-sudo apt-get install zsh -y
-sudo apt-get install vim -y
-sudo apt-get install xsel xclip -y
-sudo apt-get install atool -y
-sudo apt-get install tmux -y
-sudo apt-get install compizconfig-settings-manager compiz-plugins-extra -y
-sudo apt-get install cowsay fortune -y
+which aptitude &>/dev/null || sudo apt-get install aptitude -y
+which zsh &>/dev/null || sudo apt-get install zsh -y
+which vim &>/dev/null || sudo apt-get install vim -y
+which xsel &>/dev/null || sudo apt-get install xsel -y
+which xclip &>/dev/null || sudo apt-get install xclip -y
+which atool &>/dev/null || sudo apt-get install atool -y
+which tmux &>/dev/null || sudo apt-get install tmux -y
+which compiz &>/dev/null || sudo apt-get install compizconfig-settings-manager compiz-plugins-extra -y
+which cowsay &>/dev/null || sudo apt-get install cowsay -y
+which fortune &>/dev/null || sudo apt-get install fortune -y
 
 # python
-sudo easy_install pip
-sudo pip install -U pip setuptools distribute
+if ! which pip &>/dev/null; then
+  sudo easy_install pip
+  sudo pip install -U pip setuptools distribute
+fi
 
 # scientific tools
-sudo pip install numpy
-sudo pip install scipy
-sudo pip install scikit-learn
-sudo pip install scikit-image
+pip show numpy &>/dev/null || sudo pip install numpy
+pip show scipy &>/dev/null || sudo pip install scipy
+pip show scikit-learn &>/dev/null || sudo pip install scikit-learn
+pip show scikit-image &>/dev/null || sudo pip install scikit-image
 
 # ros tools
-sudo pip install wstool
-sudo pip install catkin_tools
+pip show wstool &>/dev/null || sudo pip install wstool
+pip show catkin-tools &>/dev/null || sudo pip install catkin-tools
