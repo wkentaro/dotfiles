e176664c3ec10a324e5536f2cc2b2562292a684f
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-12-21 20:18:52 +0900

    Add dark solarized high contrast

---
+++ scripts/set_gnome_solarized_dark_high_constrast.sh
@@ -0,0 +1,5 @@
+# Untitled.itermcolors
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/foreground_color --type string "#9BC1C2"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_color --type string "#001E26"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/bold_color --type string "#B4D4D2"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/palette --type string "#002731:#D01B24:#6BBE6C:#A57705:#2075C7:#C61B6E:#259185:#E9E2CB:#006388:#F4153B:#50EE84:#B17E28:#178DC7:#E14D8E:#00B29E:#FCF4DC"
