148ed49087ad0589df0a94087192ea1925ff45b9
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-03-29 00:30:31 +0900

    Add nd

--- sh/plugins/notes.sh
+++ sh/plugins/notes.sh
@@ -7,3 +7,13 @@ n () {
 nls () {
   find ~/notes -iname '*.rst' | egrep -v 'README.rst$'
 }
+
+nd () {
+  to_dir="$HOME/notes/2016_onedo/daily_report"
+  date=$(date +%Y-%m-%d)
+  if [ ! -e ${to_dir}/${date}.rst ]; then
+    touch ${to_dir}/${date}.rst
+    echo "$date\n==========" > "${to_dir}/${date}.rst"
+  fi
+  vim +3 ${to_dir}/$(date +%Y-%m-%d).rst
+}
