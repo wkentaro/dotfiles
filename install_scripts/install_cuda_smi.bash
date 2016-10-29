ddaf20373f08a222455e00eb6439b0086b44a7e1
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-10-30 02:47:20 +0900

    Update cuda-smi

--- install_scripts/install_cuda_smi.bash
+++ install_scripts/install_cuda_smi.bash
@@ -11,8 +11,9 @@ fi
 TMPDIR=$(mktemp -d)
 cd $TMPDIR
 
-git clone https://github.com/wkentaro/cuda-smi.git -b fix-1
+git clone https://github.com/al42and/cuda-smi.git
 cd cuda-smi
+git checkout 58c9cbc16d3f4e070cb061cdae4550bf1c6c70ea
 
 make
 cp $(pwd)/cuda-smi $HOME/.local/bin/cuda-smi
