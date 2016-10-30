54171af8296fb63bcb99823a4e54a698ae1ab244
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-10-30 16:22:47 +0900

    Update install_cuda_smi for macOS

--- install_scripts/install_cuda_smi.bash
+++ install_scripts/install_cuda_smi.bash
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-if [ "$(uname)" != "Linux" ]; then
-  exit 0
-fi
-
 if which cuda-smi &>/dev/null; then
   exit 0
 fi
 
-TMPDIR=$(mktemp -d)
-cd $TMPDIR
+TMPDIR=""
 
-git clone https://github.com/al42and/cuda-smi.git
-cd cuda-smi
-git checkout 58c9cbc16d3f4e070cb061cdae4550bf1c6c70ea
+if [ "$(uname)" = "Linux" ]; then
+  TMPDIR=$(mktemp -d)
+  cd $TMPDIR
+
+  git clone https://github.com/al42and/cuda-smi.git
+  cd cuda-smi
+  git checkout 58c9cbc16d3f4e070cb061cdae4550bf1c6c70ea
+elif [ "$(uname)" = "Darwin" ]; then
+  TMPDIR=$(mktemp -d /tmp/tmp.XXXX)
+  cd $TMPDIR
+
+  git clone https://github.com/wkentaro/osx-cuda-smi.git
+  cd osx-cuda-smi
+  git checkout v1.0
+else
+  echo "[install_cuda_smi.bash] Unsupported arch: $(uname)"
+  exit 1
+fi
 
 make
 cp $(pwd)/cuda-smi $HOME/.local/bin/cuda-smi
