1f6e8a4a1acd0f741895a6c6a6610f64497116d1
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-11-04 13:57:48 +0900

    Update cuda-smi to use al42and's

--- install_scripts/install_cuda_smi.bash
+++ install_scripts/install_cuda_smi.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if which nvcc &>/dev/null; then
+  exit 0
+fi
+
 if which cuda-smi &>/dev/null; then
   exit 0
 fi
@@ -8,22 +12,16 @@ TMPDIR=""
 
 if [ "$(uname)" = "Linux" ]; then
   TMPDIR=$(mktemp -d)
-  cd $TMPDIR
-
-  git clone https://github.com/al42and/cuda-smi.git
-  cd cuda-smi
-  git checkout 58c9cbc16d3f4e070cb061cdae4550bf1c6c70ea
 elif [ "$(uname)" = "Darwin" ]; then
   TMPDIR=$(mktemp -d /tmp/tmp.XXXX)
-  cd $TMPDIR
-
-  git clone https://github.com/wkentaro/mac-cuda-smi.git
-  cd mac-cuda-smi
-  git checkout v1.1
 else
   echo "[install_cuda_smi.bash] Unsupported arch: $(uname)"
   exit 1
 fi
+
+git clone https://github.com/al42and/cuda-smi.git -b mac-build
+cd cuda-smi
+git checkout 58c9cbc16d3f4e070cb061cdae4550bf1c6c70ea
 
 make
 cp $(pwd)/cuda-smi $HOME/.local/bin/cuda-smi
