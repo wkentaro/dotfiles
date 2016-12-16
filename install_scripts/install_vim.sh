3d493c400dcf3473e7835fda2d36156cb62abdbc
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-12-16 22:41:49 +0900

    Fix installation of vim to user space

--- install_scripts/install_vim.sh
+++ install_scripts/install_vim.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 
-cd /tmp
-wget https://github.com/vim/vim/archive/v7.4.865.tar.gz
-tar zxvf v7.4.865.tar.gz
-cd vim-7.4.865/src
+TMPDIR=$(mktemp -d)
+
+cd $TMPDIR
+
+git clone https://github.com/vim/vim.git -b v8.0.0075
+cd vim/src
+
 make distclean
-./configure
-make
+
+./configure \
+    --with-features=huge \
+    --enable-multibyte \
+    --enable-rubyinterp \
+    --enable-pythoninterp \
+    --enable-largefile \
+    --disable-netbeans \
+    --with-python-config-dir=/usr/lib/python2.7/config \
+    --enable-perlinterp \
+    --enable-luainterp \
+    --with-luajit \
+    --enable-gui=auto \
+    --enable-fail-if-missing \
+    --with-lua-prefix=/usr/include/lua5.1 \
+    --enable-cscope \
+    --prefix=~/.local
+
+make VIMRUNTIMEDIR=~/.local/share/vim/vim80
+
 make install prefix=~/.local
