0e2baf5c8a46b3917204abd4ba6f0fe821eda882
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-12-20 03:02:37 +0900

    Fix vim installation in user space

--- install_scripts/install_vim.sh
+++ install_scripts/install_vim.sh
@@ -9,22 +9,16 @@ cd vim/src
 
 make distclean
 
-./configure \
-    --with-features=huge \
-    --enable-multibyte \
-    --enable-rubyinterp \
-    --enable-pythoninterp \
-    --enable-largefile \
-    --disable-netbeans \
-    --with-python-config-dir=/usr/lib/python2.7/config \
-    --enable-perlinterp \
-    --enable-luainterp \
-    --with-luajit \
-    --enable-gui=auto \
-    --enable-fail-if-missing \
-    --with-lua-prefix=/usr/include/lua5.1 \
-    --enable-cscope \
-    --prefix=~/.local
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=/usr/lib/python2.7/config \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=/usr/lib/python3.5/config \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
 
 make VIMRUNTIMEDIR=~/.local/share/vim/vim80
 
