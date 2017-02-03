391fb0596266159733d10a06deb7fac1d26b4373
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-02-03 16:44:26 +0900

    Update ubuntu alias

--- sh/plugins/ubuntu.sh
+++ sh/plugins/ubuntu.sh
@@ -13,7 +13,7 @@ alias app='sudo aptitude purge'
 compdef _app app='sudo aptitude purge'
 alias apu='sudo aptitude update'
 compdef _apu apu='sudo aptitude update'
-alias apuu='sudo aptitude update && sudo aptitude safe-upgrade'
+alias apuu='sudo aptitude update && sudo aptitude safe-upgrade && sudo apt-get autoremove && sudo apt-get autoclean'
 compdef _apuu apuu='sudo aptitude update && sudo aptitude upgrade'
 
 alias allpkgs='aptitude search -F "%p" --disable-columns $1'
