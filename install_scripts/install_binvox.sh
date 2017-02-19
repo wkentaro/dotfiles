e5ba45590b5878d589ec7750657f8543793bb208
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-02-20 00:19:05 +0900

    Executable install_binvox.sh

---
+++ install_scripts/install_binvox.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$(uname)" = "Darwin" ]; then
+  exit 1
+fi
+
+if [ ! -f $HOME/.local/bin/binvox ]; then
+  wget http://www.patrickmin.com/binvox/linux64/binvox -O $HOME/.local/bin/binvox
+  chmod u+x $HOME/.local/bin/binvox
+fi
+
+if [ ! -f $HOME/.local/bin/viewvox ]; then
+  wget http://www.patrickmin.com/viewvox/linux64/viewvox -O $HOME/.local/bin/viewvox
+  chmod u+x $HOME/.local/bin/viewvox
+fi
