fc9fa3e6c784302d7ae31893c56f0aae87db5c3a
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-02-27 05:02:43 +0900

    Add g1hash

--- sh/plugins/git.sh
+++ sh/plugins/git.sh
@@ -11,6 +11,7 @@ alias grh~1='git reset HEAD~1'
 alias grh~2='git reset HEAD~2'
 alias grh~3='git reset HEAD~3'
 alias g1msg='git log -1 --format="%s" | cat'
+alias g1hash='git log -1 --format="%h" | cat'
 alias gram='git remote add ${GITHUB_USER} && git fetch ${GITHUB_USER}'
 
 # Use hub as git client
