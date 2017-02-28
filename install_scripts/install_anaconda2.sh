11c5bbc54c67a72957b155d05f5969b7ad855695
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-02-28 13:56:28 +0900

    Add anaconda scripts for mac

--- install_scripts/install_anaconda2.sh
+++ install_scripts/install_anaconda2.sh
@@ -5,7 +5,14 @@ if [ -e ~/.anaconda2 ]; then
 fi
 
 cd ~/Downloads
-if [ ! -e Anaconda2-4.3.0-Linux-x86_64.sh ]; then
-  wget 'https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh'
+if [ "$(uname)" = "Linux" ]; then
+  if [ ! -e Anaconda2-4.3.0-Linux-x86_64.sh ]; then
+    wget 'https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh'
+  fi
+  bash ./Anaconda2-4.3.0-Linux-x86_64.sh -p $HOME/.anaconda2 -b
+elif [ "$(uname)" = "Darwin" ]; then
+  if [ ! -e Anaconda2-4.3.0-MacOSX-x86_64.sh ]; then
+    wget 'https://repo.continuum.io/archive/Anaconda2-4.3.0-MacOSX-x86_64.sh'
+  fi
+  bash ./Anaconda2-4.3.0-MacOSX-x86_64.sh -p $HOME/.anaconda2 -b
 fi
-bash ./Anaconda2-4.3.0-Linux-x86_64.sh -p $HOME/.anaconda2 -b
