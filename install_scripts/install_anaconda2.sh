54f11517eb625649f14c9b6299cc3170db73f9c3
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-04-23 18:54:09 +0900

    Upgrade anaconda

--- install_scripts/install_anaconda2.sh
+++ install_scripts/install_anaconda2.sh
@@ -4,15 +4,16 @@ if [ -e ~/.anaconda2 ]; then
   exit 0
 fi
 
+VERSION=4.3.1
 cd ~/Downloads
 if [ "$(uname)" = "Linux" ]; then
-  if [ ! -e Anaconda2-4.3.0-Linux-x86_64.sh ]; then
-    wget 'https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh'
+  if [ ! -e Anaconda2-${VERSION}-Linux-x86_64.sh ]; then
+    wget "https://repo.continuum.io/archive/Anaconda2-${VERSION}-Linux-x86_64.sh"
   fi
-  bash ./Anaconda2-4.3.0-Linux-x86_64.sh -p $HOME/.anaconda2 -b
+  bash ./Anaconda2-${VERSION}-Linux-x86_64.sh -p $HOME/.anaconda2 -b
 elif [ "$(uname)" = "Darwin" ]; then
-  if [ ! -e Anaconda2-4.3.0-MacOSX-x86_64.sh ]; then
-    wget 'https://repo.continuum.io/archive/Anaconda2-4.3.0-MacOSX-x86_64.sh'
+  if [ ! -e Anaconda2-${VERSION}-MacOSX-x86_64.sh ]; then
+    wget "https://repo.continuum.io/archive/Anaconda2-${VERSION}-MacOSX-x86_64.sh"
   fi
-  bash ./Anaconda2-4.3.0-MacOSX-x86_64.sh -p $HOME/.anaconda2 -b
+  bash ./Anaconda2-${VERSION}-MacOSX-x86_64.sh -p $HOME/.anaconda2 -b
 fi
