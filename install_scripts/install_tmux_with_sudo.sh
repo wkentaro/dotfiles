7f96fecdd5718f70681571d58cbd8613f207a7e2
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-04-29 17:21:40 +0000

    Update install_tmux_with_sudo

--- install_scripts/install_tmux_with_sudo.sh
+++ install_scripts/install_tmux_with_sudo.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
+if [ "$(uname)" != "Linux" ]; then
+  exit 1
+fi
+
 if [ -f /usr/local/bin/tmux ]; then
   exit 0
 fi
 
-sudo aptitude install libevent-dev
+sudo -H apt-get install -qq -y libevent-dev
 
-cd /tmp
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
 
 wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
-tar zxvf tmux-2.2.tar.gz
+tar zxf tmux-2.2.tar.gz
 cd tmux-2.2
+
 ./configure
 make
 sudo make install
+
+rm -rf $TMPDIR
