775fd9f3e69d3620c27080f7f318dd86ce4bcaa9
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-04-30 02:58:47 +0900

    Install /usr/local/bin hub

--- install_scripts/install_hub.sh
+++ install_scripts/install_hub.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-if [ -f $HOME/.local/bin/hub ]; then
+if [ -f /usr/local/bin/hub ]; then
   exit 0
 elif [ "$(uname)" = "Darwin" ]; then
   exit 1
 fi
 
 sudo mkdir -p /usr/local/share/man/man1
-mkdir -p ~/.local/bin
 
 TMPDIR=$(mktemp -d)
 
@@ -16,7 +15,7 @@ url="https://github.com/github/hub/releases/download/v2.2.2/hub-linux-amd64-2.2.
 wget $url -O /tmp/hub-linux-amd64-2.2.2.tgz
 tar zxf hub-linux-amd64-2.2.2.tgz
 
-cp hub-linux-amd64-2.2.2/bin/hub $HOME/.local/bin/hub
+cp hub-linux-amd64-2.2.2/bin/hub /usr/local/bin/hub
 sudo cp hub-linux-amd64-2.2.2/etc/hub.bash_completion.sh /etc/bash_completion.d/hub.bash_completion.sh
 sudo cp hub-linux-amd64-2.2.2/etc/hub.zsh_completion /usr/local/share/zsh/site-functions/_hub
 sudo cp hub-linux-amd64-2.2.2/share/man/man1/hub.1 /usr/local/share/man/man1/hub.1
