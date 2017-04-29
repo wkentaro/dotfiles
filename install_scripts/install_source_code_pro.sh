c66335f4881e325cfc0293fbcc3764ce327948be
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-04-30 04:32:04 +0900

    Quiet for source_code_pro

--- install_scripts/install_source_code_pro.sh
+++ install_scripts/install_source_code_pro.sh
@@ -18,7 +18,7 @@ TMPDIR=$(mktemp -d)
 cd $TMPDIR
 
 wget https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip
-unzip 1.030R-it.zip
+unzip -qq 1.030R-it.zip
 
 mkdir -p ~/.fonts
 cp source-code-pro-2.010R-ro-1.030R-it/OTF/*.otf ~/.fonts/
