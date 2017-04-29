902aed006f69981cb78d3826fe4871b7594933cf
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-04-30 02:55:37 +0900

    Fix install_hub

--- install_scripts/install_hub.sh
+++ install_scripts/install_hub.sh
@@ -7,12 +7,18 @@ elif [ "$(uname)" = "Darwin" ]; then
 fi
 
 sudo mkdir -p /usr/local/share/man/man1
+mkdir -p ~/.local/bin
 
+TMPDIR=$(mktemp -d)
+
+cd $TMPDIR
 url="https://github.com/github/hub/releases/download/v2.2.2/hub-linux-amd64-2.2.2.tgz"
 wget $url -O /tmp/hub-linux-amd64-2.2.2.tgz
-cd /tmp
-tar zxvf hub-linux-amd64-2.2.2.tgz
+tar zxf hub-linux-amd64-2.2.2.tgz
+
 cp hub-linux-amd64-2.2.2/bin/hub $HOME/.local/bin/hub
 sudo cp hub-linux-amd64-2.2.2/etc/hub.bash_completion.sh /etc/bash_completion.d/hub.bash_completion.sh
 sudo cp hub-linux-amd64-2.2.2/etc/hub.zsh_completion /usr/local/share/zsh/site-functions/_hub
 sudo cp hub-linux-amd64-2.2.2/share/man/man1/hub.1 /usr/local/share/man/man1/hub.1
+
+rm -rf $TMPDIR
