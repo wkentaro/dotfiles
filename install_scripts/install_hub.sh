0f37d7ef5813439f914e6029cc8552f575f302f3
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-04-30 14:43:37 +0000

    Use hub 2.2.9

--- install_scripts/install_hub.sh
+++ install_scripts/install_hub.sh
@@ -11,13 +11,13 @@ sudo mkdir -p /usr/local/share/man/man1
 TMPDIR=$(mktemp -d)
 
 cd $TMPDIR
-URL='https://github.com/github/hub/releases/download/v2.2.2/hub-linux-amd64-2.2.2.tgz'
-wget -q $URL -O hub-linux-amd64-2.2.2.tgz
-tar zxf hub-linux-amd64-2.2.2.tgz
+URL='https://github.com/github/hub/releases/download/v2.2.9/hub-linux-amd64-2.2.9.tgz'
+wget -q $URL -O hub-linux-amd64-2.2.9.tgz
+tar zxf hub-linux-amd64-2.2.9.tgz
 
-cp hub-linux-amd64-2.2.2/bin/hub /usr/local/bin/hub
-sudo cp hub-linux-amd64-2.2.2/etc/hub.bash_completion.sh /etc/bash_completion.d/hub.bash_completion.sh
-sudo cp hub-linux-amd64-2.2.2/etc/hub.zsh_completion /usr/local/share/zsh/site-functions/_hub
-sudo cp hub-linux-amd64-2.2.2/share/man/man1/hub.1 /usr/local/share/man/man1/hub.1
+sudo cp hub-linux-amd64-2.2.9/bin/hub /usr/local/bin/hub
+sudo cp hub-linux-amd64-2.2.9/etc/hub.bash_completion.sh /etc/bash_completion.d/hub.bash_completion.sh
+sudo cp hub-linux-amd64-2.2.9/etc/hub.zsh_completion /usr/local/share/zsh/site-functions/_hub
+sudo cp hub-linux-amd64-2.2.9/share/man/man1/hub.1 /usr/local/share/man/man1/hub.1
 
 rm -rf $TMPDIR
