1e4e1f696563190cca0d9bf2a9b77444c6352f0a
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-04-30 10:31:16 +0000

    Update to install latex

--- install_scripts/install_latex.sh
+++ install_scripts/install_latex.sh
@@ -10,9 +10,13 @@ if [ "`uname`" != "Linux" ]; then
 fi
 
 
-sudo apt-get -y install --reinstall texlive
-sudo apt-get -y install --reinstall texlive-lang-cjk
-sudo apt-get -y install --reinstall xdvik-ja
-sudo apt-get -y install --reinstall dvipsk-ja
-sudo apt-get -y install --reinstall gv
-sudo apt-get -y install --reinstall texlive-fonts-recommended texlive-fonts-extra
+sudo apt-get install -qq -y texlive
+sudo apt-get install -qq -y texlive-lang-cjk
+sudo apt-get install -qq -y xdvik-ja
+sudo apt-get install -qq -y dvipsk-ja
+sudo apt-get install -qq -y gv
+sudo apt-get install -qq -y texlive-fonts-recommended texlive-fonts-extra
+
+if [ "$(lsb_release -cs)" = "xenial" ]; then
+  sudo apt-get install -qq -y latexmk
+fi
