4f12a4d56d35320cb9fde191795f0ce64f138aad
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-07-06 20:06:03 +0900

    Update install_diff_highlight

--- install_scripts/install_diff_highlight.sh
+++ install_scripts/install_diff_highlight.sh
@@ -8,6 +8,13 @@ if type diff-highlight &>/dev/null; then
   exit 0
 fi
 
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
+
+wget https://github.com/git/git/archive/v2.13.2.tar.gz
+tar zxvf v2.13.2.tar.gz
+cd git-2.13.2/contrib/diff-highlight
+make
+
 mkdir -p $HOME/.local/bin
-wget -q https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight -O $HOME/.local/bin/diff-highlight --no-check-certificate
-chmod u+x $HOME/.local/bin/diff-highlight
+mv diff-highlight $HOME/.local/bin
