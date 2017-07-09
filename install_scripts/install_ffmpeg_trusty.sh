e455c503d44bd93d44065d111a458b2c404d2b93
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-07-10 02:46:07 +0900

    set -x to install_ffmpeg

--- install_scripts/install_ffmpeg_trusty.sh
+++ install_scripts/install_ffmpeg_trusty.sh
@@ -12,7 +12,11 @@ if which ffmpeg &>/dev/null; then
   exit 1
 fi
 
+set -x
+
 sudo apt-get install -qq -y software-properties-common
 sudo add-apt-repository ppa:mc3man/trusty-media -y
 sudo apt-get update -qq
 sudo apt-get install -qq -y ffmpeg
+
+set +x
