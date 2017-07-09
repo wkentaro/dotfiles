788415a649d80df3cf92111c4fec50553e00c645
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-07-09 18:55:18 +0900

    Update install_ros_indigo.sh

--- install_scripts/install_ros_indigo.sh
+++ install_scripts/install_ros_indigo.sh
@@ -5,7 +5,7 @@ if ! which lsb_release >/dev/null; then
   sudo -H apt-get install -qq -y software-properties-common
 fi
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros-shadow-fixed/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo -H apt-get update -q -y
 
