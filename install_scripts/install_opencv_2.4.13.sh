9bd4ecf73e7f600b8ce0aceadf92b71a2b4f5e2d
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-08-14 04:37:59 +0900

    Fix installation of cv2.so

--- install_scripts/install_opencv_2.4.13.sh
+++ install_scripts/install_opencv_2.4.13.sh
@@ -28,6 +28,7 @@ cd release
 cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=/usr/bin/g++ CMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_TBB=ON -DBUILD_NEW_PYTHON_SUPPORT=ON -DWITH_V4L=ON -DINSTALL_C_EXAMPLES=ON -DINSTALL_PYTHON_EXAMPLES=ON -DBUILD_EXAMPLES=ON -DWITH_QT=ON -DWITH_OPENGL=ON -DBUILD_FAT_JAVA_LIB=ON -DINSTALL_TO_MANGLED_PATHS=ON -DINSTALL_CREATE_DISTRIB=ON -DINSTALL_TESTS=ON -DENABLE_FAST_MATH=ON -DWITH_IMAGEIO=ON -DBUILD_SHARED_LIBS=OFF -DWITH_GSTREAMER=ON ..
 make all -j2 # 2 cores
 sudo make install
+sudo cp /usr/local/python/2.7/cv2.so /usr/local/lib/python2.7/dist-packages/cv2.so
 
 # ignore libdc1394 error http://stackoverflow.com/questions/12689304/ctypes-error-libdc1394-error-failed-to-initialize-libdc1394
 
