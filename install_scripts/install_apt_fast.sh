9a7bdd81dd5d681bcc51ab4c6cf48daebaf62aea
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-08-20 16:13:24 +0900

    Non interactive apt_fast

--- install_scripts/install_apt_fast.sh
+++ install_scripts/install_apt_fast.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 if hash apt-fast &>/dev/null; then
-  echo "apt-fast is already installed"
   exit 0
 fi
 
-sudo apt-get install -qq -y software-properties-common
-sudo add-apt-repository ppa:saiarcot895/myppa -y
-sudo apt-get update -qq
-sudo apt-get install -qq -y apt-fast
+sudo add-apt-repository ppa:saiarcot895/myppa < /dev/null
+sudo apt-get update
+echo debconf apt-fast/maxdownloads string 16 | sudo debconf-set-selections
+echo debconf apt-fast/dlflag boolean true | sudo debconf-set-selections
+echo debconf apt-fast/aptmanager string apt-get | sudo debconf-set-selections
+sudo apt-get install -y apt-fast
