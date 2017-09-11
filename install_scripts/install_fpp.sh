3582dafe533041ca069b12854462f0a11aae1370
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-09-12 03:34:17 +0900

    Fix install_fpp.sh

--- install_scripts/install_fpp.sh
+++ install_scripts/install_fpp.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -f ~/.local/bin/fpp ]; then
+  echo "[$(basename $0)] fpp is already installed."
   exit 0
 fi
+
+set -x
 
 mkdir -p ~/.local/share
 cd ~/.local/share
 git clone https://github.com/facebook/PathPicker.git
 ln -s $(pwd)/PathPicker/fpp ~/.local/bin/fpp
+
+set +x
