605e77f4e96dbb61bfd4659ce71517786407ae82
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-09-12 02:56:53 +0900

    Fix install_docker.sh

--- install_scripts/install_docker.sh
+++ install_scripts/install_docker.sh
@@ -1,13 +1,10 @@
-#!/bin/sh
-
-# docker
-# ~~~~~~
-
+#!/bin/bash
 
 if [ "`uname`" != "Linux" ]; then
-  exit 1
+  exit 0
 fi
 
+set -x
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
@@ -19,3 +16,5 @@ sudo usermod -aG docker $(whoami)
 
 # install using dockers
 docker pull ubuntu
+
+set +x
