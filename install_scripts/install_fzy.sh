82a6b7340c2a65dce4f16921622b02233954ae69
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-10-07 15:28:54 +0900

    Add install_fzy.sh

---
+++ install_scripts/install_fzy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if which fzy &>/dev/null; then
+  exit 0
+fi
+
+set -x
+
+TMPDIR=$(mktemp -d)
+
+cd $TMPDIR
+VERSION=0.9
+wget -q https://github.com/jhawthorn/fzy/archive/${VERSION}.tar.gz
+tar zxf ${VERSION}.tar.gz
+cd fzy-${VERSION}
+
+make PREFIX=$HOME/.local
+make install DESTDIR=$HOME/.local
+
+set +x
