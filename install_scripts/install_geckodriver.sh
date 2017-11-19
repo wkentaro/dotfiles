080173a9d3b1552996f81137f560bd1c5774282d
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-11-19 20:50:22 +0900

    Install geckodriver

---
+++ install_scripts/install_geckodriver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+if [ "$(uname)" = "Linux" ]; then
+  URL='https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz'
+elif [ "$(uname)" = "Darwin" ]; then
+  URL='https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-macos.tar.gz'
+fi
+
+TMP_DIR=$(mktemp -d)
+
+cd $TMP_DIR
+wget $URL -O geckodriver.tar.gz
+tar zxvf geckodriver.tar.gz
+mv geckodriver $HOME/.local/bin/geckodriver
+cd -
+
+rm -rf $TMP_DIR
