76078b6007f5ef97d07a4c8dd0e4196dba5af4e5
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-11-20 20:09:26 +0900

    Update scripts/update_dotfiles.sh

--- scripts/update_dotfiles.sh
+++ scripts/update_dotfiles.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-for server in localhost jenkins jhoop jdlbox1 jdlbox2 jdlbox3 jdlbox4 jdlbox5 adlbox6 jbaxter-c1; do
+for server in localhost jenkins jhoop jdlbox1 jdlbox2 jdlbox3 jdlbox4 jdlbox5 crux jbaxter-c1; do
   timeout 3 ssh $server true && \
     ssh $server '(cd .dotfiles && git pull origin master && zsh -c "source ~/.zshrc && ./install.py")' &
 done
