83a3e2697926a3370fa5eb49d9dd9447c6ee116f
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-12-03 17:05:03 +0900

    Upgrade tmux: 2.5 -> 2.6

--- install_scripts/install_tmux.sh
+++ install_scripts/install_tmux.sh
@@ -13,7 +13,7 @@ sudo -H apt-get install -qq -y libevent-dev libncurses-dev
 TMPDIR=$(mktemp -d)
 cd $TMPDIR
 
-VERSION=2.5
+VERSION=2.6
 wget -q https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
 tar zxf tmux-${VERSION}.tar.gz
 cd tmux-${VERSION}
