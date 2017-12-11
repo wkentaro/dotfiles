3c249b5c1a7ceffe369cf63d51b7f64a0c773321
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-12-11 20:03:05 +0900

    Add install_anaconda3.sh

---
+++ local/bin/install_anaconda3.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ ! $# -eq 1 ]; then
+  echo "Usage: $0 INSTALL_DIR"
+  exit 1
+fi
+
+INSTALL_DIR=$1
+INSTALL_DIR=$(cd $INSTALL_DIR && pwd)
+
+if [ -e $INSTALL_DIR/.anaconda3 ]; then
+  echo "Anaconda3 is already installed: $INSTALL_DIR/.anaconda3"
+  exit 0
+fi
+
+set -x
+
+cd $(mktemp -d)
+
+if [ "$(uname)" = "Linux" ]; then
+  wget -q 'https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh'
+  bash ./Miniconda3-latest-Linux-x86_64.sh -p $INSTALL_DIR/.anaconda3 -b
+elif [ "$(uname)" = "Darwin" ]; then
+  wget -q 'https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh'
+  bash ./Miniconda3-latest-MacOSX-x86_64.sh -p $INSTALL_DIR/.anaconda3 -b
+else
+  echo "[$(basename $0)] Unsupported platform: $(uname)"
+  exit 0
+fi
+
+set +x
