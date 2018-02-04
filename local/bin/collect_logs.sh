fc68a006445db9126a96a8988c23aac7d4865ff8
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-02-04 10:29:03 +0900

    Update local/bin/collect_logs.sh

--- local/bin/collect_logs.sh
+++ local/bin/collect_logs.sh
@@ -8,7 +8,7 @@ if [ ! -e logs ]; then
   exit 1
 fi
 
-for server in green dlboxs1 dlbox1 dlbox2 dlbox3 dlbox4 dlbox5; do
+for server in green dlboxs1 dlbox1 dlbox2 dlbox3 dlbox4 dlbox5 dlbox6 dlbox7 dlbox8 dlbox9; do
   echo "[$server] Synchronizing $(pwd)/logs ..."
   timeout 1 ssh $server ls &>/dev/null && rsync -at $server:$(pwd)/logs/ logs/ 2>/dev/null &
 done
