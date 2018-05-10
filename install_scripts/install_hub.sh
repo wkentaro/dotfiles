a901b8ad67447b023ce331edcf3526186c9910b1
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-05-10 14:28:02 +0900

    Update install_scripts/install_hub.sh

--- install_scripts/install_hub.sh
+++ install_scripts/install_hub.sh
@@ -9,15 +9,16 @@ fi
 sudo mkdir -p /usr/local/share/man/man1
 
 TMPDIR=$(mktemp -d)
+VERSION='2.3.0-pre10'
 
 cd $TMPDIR
-URL='https://github.com/github/hub/releases/download/v2.2.9/hub-linux-amd64-2.2.9.tgz'
-wget -q $URL -O hub-linux-amd64-2.2.9.tgz
-tar zxf hub-linux-amd64-2.2.9.tgz
+URL="https://github.com/github/hub/releases/download/v$VERSION/hub-linux-amd64-$VERSION.tgz"
+wget -q $URL -O hub-linux-amd64-$VERSION.tgz
+tar zxf hub-linux-amd64-$VERSION.tgz
 
-sudo cp hub-linux-amd64-2.2.9/bin/hub /usr/local/bin/hub
-sudo cp hub-linux-amd64-2.2.9/etc/hub.bash_completion.sh /etc/bash_completion.d/hub.bash_completion.sh
-sudo cp hub-linux-amd64-2.2.9/etc/hub.zsh_completion /usr/local/share/zsh/site-functions/_hub
-sudo cp hub-linux-amd64-2.2.9/share/man/man1/hub.1 /usr/local/share/man/man1/hub.1
+sudo cp hub-linux-amd64-$VERSION/bin/hub /usr/local/bin/hub
+sudo cp hub-linux-amd64-$VERSION/etc/hub.bash_completion.sh /etc/bash_completion.d/hub.bash_completion.sh
+sudo cp hub-linux-amd64-$VERSION/etc/hub.zsh_completion /usr/local/share/zsh/site-functions/_hub
+sudo cp hub-linux-amd64-$VERSION/share/man/man1/hub.1 /usr/local/share/man/man1/hub.1
 
 rm -rf $TMPDIR
