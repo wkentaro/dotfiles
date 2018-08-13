27e261604e1ad33949d330281972a69ed962df16
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-08-13 23:50:24 +0900

    Update install_scripts/install_vim.nosudo.opt.sh

--- install_scripts/install_vim.nosudo.opt.sh
+++ install_scripts/install_vim.nosudo.opt.sh
@@ -19,9 +19,9 @@ make distclean
             --enable-multibyte \
             --enable-rubyinterp=yes \
             --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config \
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
             --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.5/config \
+            --with-python3-config-dir=/usr/lib/python3.5/config-x86_64-linux-gnu \
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
             --enable-gui=gtk2 --enable-cscope --prefix=/usr
