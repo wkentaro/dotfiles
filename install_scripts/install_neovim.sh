40b36317692b248fe666fba5c93d9463c4206c8a
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-08-29 03:46:47 +0900

    Add install_neovim.sh

---
+++ install_scripts/install_neovim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$(uname)" != "Linux" ]; then
+  exit 1
+fi
+
+set -e
+set -x
+
+sudo apt-get install software-properties-common
+
+sudo apt-add-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+
+sudo apt-get install python-dev python-pip python3-dev python3-pip
