a142967a2f63d480dc4865de7a475cb32f89b0a4
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-11-12 00:30:10 +0000

    Update local/bin/install_anaconda3.sh

--- local/bin/install_anaconda3.sh
+++ local/bin/install_anaconda3.sh
@@ -16,15 +16,15 @@ fi
 cd $(mktemp -d)
 
 if [ "$(uname)" = "Linux" ]; then
-  wget -q 'https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh'
-  bash ./Miniconda3-latest-Linux-x86_64.sh -p $INSTALL_DIR/.anaconda3 -b
+  wget -q 'https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh' -O miniconda3.sh
 elif [ "$(uname)" = "Darwin" ]; then
   wget -q 'https://repo.continuum.io/miniconda/Miniconda3-4.5.4-MacOSX-x86_64.sh'
-  bash ./Miniconda3-latest-MacOSX-x86_64.sh -p $INSTALL_DIR/.anaconda3 -b
 else
   echo "[$(basename $0)] Unsupported platform: $(uname)"
   exit 0
 fi
+
+bash ./miniconda3.sh -p $INSTALL_DIR/.anaconda3 -b
 
 source $INSTALL_DIR/.anaconda3/bin/activate
 conda update -n base -y conda
