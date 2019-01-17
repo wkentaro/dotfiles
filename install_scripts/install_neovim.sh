dbce0146597506151de28dfd4a7e5d9f8074e957
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-01-17 15:07:49 +0000

    Update install_scripts/install_neovim.sh

--- install_scripts/install_neovim.sh
+++ install_scripts/install_neovim.sh
@@ -7,12 +7,12 @@ fi
 set -e
 set -x
 
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 
-sudo apt-add-repository ppa:neovim-ppa/stable
+sudo apt-add-repository -y ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install -y neovim
 
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip
 
 sudo pip install neovim jedi
