9cd2ede7dfafac17dd9801d914ff754ff324a21d
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-03-11 17:46:57 +0000

    Add pskill

--- sh/rc/alias.sh
+++ sh/rc/alias.sh
@@ -280,7 +280,12 @@ psf () {
     echo "Usage: $0 PATTERN"
     return 1
   fi
-  ps auxwww | grep $1
+  ps auxwww | grep $1 | grep -v grep
+}
+pskill() {
+  while read data; do
+    echo $data | awk '{print $2}' | xargs -n1 kill -9
+  done
 }
 alias pii='pip install'
 alias piu='pip uninstall'
