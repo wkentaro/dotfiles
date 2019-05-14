c07b79a1fb34ce2e7f33cdaf3569ea9b5ab87de7
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-05-14 16:16:00 +0100

    Use latest

--- local/bin/install_anaconda3.sh
+++ local/bin/install_anaconda3.sh
@@ -17,9 +17,9 @@ TMPDIR=$(mktemp -d)
 cd $TMPDIR
 
 if [ "$(uname)" = "Linux" ]; then
-  wget -q 'https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh' -O miniconda3.sh
+  wget -q 'https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh' -O miniconda3.sh
 elif [ "$(uname)" = "Darwin" ]; then
-  wget -q 'https://repo.continuum.io/miniconda/Miniconda3-4.5.4-MacOSX-x86_64.sh' -O miniconda3.sh
+  wget -q 'https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh' -O miniconda3.sh
 else
   echo "[$(basename $0)] Unsupported platform: $(uname)"
   exit 0
