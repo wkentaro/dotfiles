7b25144b0c2f57ee6d68f8e40fc27483ad69df1a
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-07-24 16:19:56 +0100

    Update install_scripts/install_diff_highlight.sh

--- install_scripts/install_diff_highlight.sh
+++ install_scripts/install_diff_highlight.sh
@@ -7,7 +7,12 @@ fi
 TMPDIR=$(mktemp -d)
 cd $TMPDIR
 
-wget https://github.com/git/git/archive/v2.13.2.tar.gz
+URL='https://github.com/git/git/archive/v2.13.2.tar.gz'
+if which wget &>/dev/null; then
+  wget -q $URL
+else
+  curl -s -L -O $URL
+fi
 tar zxf v2.13.2.tar.gz
 cd git-2.13.2/contrib/diff-highlight
 make
