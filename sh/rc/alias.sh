839f45d6fe137bdf0d76f29ce95a3fc52142d529
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-08-13 13:43:45 +0100

    Add diff

--- sh/rc/alias.sh
+++ sh/rc/alias.sh
@@ -339,3 +339,20 @@ nhup () {
 }
 
 alias k9='kill -9'
+
+diff () {
+        if zstyle -t ':prezto:module:utility:diff' color
+        then
+                if (( $+commands[colordiff] ))
+                then
+                        command colordiff --unified "$@"
+                elif (( $+commands[git] ))
+                then
+                        git --no-pager diff --color=auto --no-ext-diff --no-index "$@"
+                else
+                        command diff --unified "$@"
+                fi
+        else
+                command diff --unified "$@"
+        fi
+}
