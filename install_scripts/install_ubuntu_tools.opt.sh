0d77675d8e0fe292cf0f2874c6a42a1d537f5881
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-11-26 20:22:30 +0000

    Update install_scripts/install_ubuntu_tools.opt.sh

--- install_scripts/install_ubuntu_tools.opt.sh
+++ install_scripts/install_ubuntu_tools.opt.sh
@@ -18,19 +18,3 @@ which cowsay &>/dev/null || sudo apt-get install -qq -y cowsay
 which fortune &>/dev/null || sudo apt-get install -qq -y fortune
 which pandoc &>/dev/null || sudo apt-get install -qq -y pandoc
 which gthumb &>/dev/null || sudo apt-get install -qq -y gthumb
-
-# python
-if ! which pip &>/dev/null; then
-  sudo easy_install pip
-  sudo pip install -U pip setuptools distribute
-fi
-
-# scientific tools
-pip show numpy &>/dev/null || sudo pip install -q numpy
-pip show scipy &>/dev/null || sudo pip install -q scipy
-pip show scikit-learn &>/dev/null || sudo pip install -q scikit-learn
-pip show scikit-image &>/dev/null || sudo pip install -q scikit-image
-
-# ros tools
-pip show wstool &>/dev/null || sudo pip install -q wstool
-pip show catkin-tools &>/dev/null || sudo pip install -q catkin-tools
