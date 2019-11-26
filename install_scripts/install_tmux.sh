109ce566beaf590e3a93d09eda1d7669b3b81a35
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-11-26 23:53:35 +0000

    Use tmux 2.9

--- install_scripts/install_tmux.sh
+++ install_scripts/install_tmux.sh
@@ -14,7 +14,7 @@ fi
 TMPDIR=$(mktemp -d)
 cd $TMPDIR
 
-VERSION=2.8
+VERSION=2.9
 wget -q https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
 tar zxf tmux-${VERSION}.tar.gz
 cd tmux-${VERSION}
