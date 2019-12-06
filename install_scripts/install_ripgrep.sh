4a41467270f739621575f4f3caf939341895322a
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-12-06 19:08:03 +0000

    Add install_ripgrep.sh

---
+++ install_scripts/install_ripgrep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if which rg &>/dev/null; then
+  echo "ripgrep is already installed"
+  exit 1
+fi
+
+set -x
+
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
+
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
+
+rm -rf $TMPDIR
