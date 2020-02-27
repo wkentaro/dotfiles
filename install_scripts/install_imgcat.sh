ad00dabb6cdce59361f4f8d7540d0dd33e908bb3
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-02-27 00:36:24 +0000

    Update install_scripts/install_imgcat.sh

--- install_scripts/install_imgcat.sh
+++ install_scripts/install_imgcat.sh
@@ -4,5 +4,4 @@ if [ -e $HOME/.local/bin/imgcat ]; then
   exit 0
 fi
 
-wget https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat -O $HOME/.local/bin/imgcat
-chmod u+x $HOME/.local/bin/imgcat
+pip3 install --user imgcat
