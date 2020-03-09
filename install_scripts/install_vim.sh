03a54bcc0f57569272263579685e7c77456ae8ec
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-03-09 20:30:36 +0000

    opt -> manual

--- install_scripts/install_vim.sh
+++ install_scripts/install_vim.sh
@@ -7,7 +7,7 @@ fi
 HERE=$(realpath $(dirname ${BASH_SOURCE[0]}))
 
 if [ "$(lsb_release -sr)" = "16.04" ]; then
-  bash $HERE/install_vim.xenial.opt.sh
+  bash $HERE/install_vim.xenial.manual.sh
 elif [ "$(lsb_release -sr)" = "18.04" ]; then
-  bash $HERE/install_vim.bionic.opt.sh
+  bash $HERE/install_vim.bionic.manual.sh
 fi
