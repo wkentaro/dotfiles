28f51aedc91ea123a6fd26819b796e76c3abc794
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-05-09 17:00:24 +0100

    Update install_scripts/install_docker.manual.sh

--- install_scripts/install_docker.manual.sh
+++ install_scripts/install_docker.manual.sh
@@ -6,10 +6,26 @@ fi
 
 set -x
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-sudo apt-get update -qq -y
+curl --fail --silent --show-error --location https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo apt-get install -qq -y lxc-docker
+sudo apt-get install software-properties-common
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+apt-cache policy docker-ce
+
+sudo apt-get install -y docker-ce
+
+sudo systemctl status docker
+
+sudo usermod -aG docker ${USER}
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
 
 set +x
