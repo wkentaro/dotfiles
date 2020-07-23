7d0df0cf9380b222b574a17fee3373023c1c7ca9
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-07-23 18:56:53 +0100

    Use jq_to_table for als

--- sh/plugins/aws.sh
+++ sh/plugins/aws.sh
@@ -1,21 +1,13 @@
 profiles=(minerva corvus)
 
-regions=(us-east-1 eu-west-1)
-
 echo_bold () {
   echo -e "\033[1m$*\033[0m"
 }
 
 als() {
-  for profile in ${profiles[@]}; do
-    echo_bold "# $profile"
-    for region in ${regions[@]}; do
-      content=$(awless --no-sync list instances -p $profile -r $region)
-      if [ $(echo $content | wc -l) -ne 1 ]; then
-        echo $content
-      fi
-    done
-    echo
+  for profile in $profiles; do
+    echo "# $profile"
+    aws ec2 describe-instances --profile $profile | jq '.Reservations[] | ( .Instances[] | {state: .State.Name, id: .InstanceId, name: .KeyName, type: .InstanceType, ip: .PublicIpAddress})' | jq_to_table
   done
 }
 ast() {
