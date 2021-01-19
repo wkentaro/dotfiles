83fbd8de34e884972ab886f7f5c644d896e13581
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-01-19 22:00:30 +0000

    Update install_scripts/install_latex.manual.sh

--- install_scripts/install_latex.manual.sh
+++ install_scripts/install_latex.manual.sh
@@ -4,14 +4,16 @@ if [ "`uname`" != "Linux" ]; then
   exit 0
 fi
 
-sudo apt-get install -qq -y texlive
-sudo apt-get install -qq -y texlive-lang-cjk
-sudo apt-get install -qq -y xdvik-ja
-sudo apt-get install -qq -y dvipsk-ja
-sudo apt-get install -qq -y gv
-sudo apt-get install -qq -y texlive-fonts-recommended texlive-fonts-extra
+set -x
+
+sudo apt-get install -y texlive
+sudo apt-get install -y texlive-lang-cjk
+sudo apt-get install -y xdvik-ja
+sudo apt-get install -y dvipsk-ja
+sudo apt-get install -y gv
+sudo apt-get install -y texlive-fonts-recommended texlive-fonts-extra
 
 if [ "$(lsb_release -cs)" = "xenial" ]; then
-  sudo apt-get install -qq -y latexmk
-  sudo apt-get install -qq -y texlive-xetex
+  sudo apt-get install -y latexmk
+  sudo apt-get install -y texlive-xetex
 fi
