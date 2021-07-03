372cb5d2714dfd63226e2f77ea1ca210a93eb65b
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-07-03 17:17:49 +0000

    Update install_scripts/install_neovim.sh

--- install_scripts/install_neovim.sh
+++ install_scripts/install_neovim.sh
@@ -15,7 +15,7 @@ sudo apt-add-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install -y neovim
 
-sudo apt-get install -y python-dev python-pip python3-dev python3-pip
+sudo apt-get install -y python3-dev python3-pip
 
 bash $HERE/install_python3.7.sh
 sudo python3.7 -m pip install neovim jedi pynvim
