5d1291b55314e9039c2983ce804ab6d225d9cd31
Kentaro Wada <kentaro.wada@mujin.co.jp>
2022-09-20 09:03:00 +0900

    Update install_scripts/install_exa.sh

--- install_scripts/install_exa.sh
+++ install_scripts/install_exa.sh
@@ -9,9 +9,12 @@ if [ -f $HOME/.local/bin/exa ]; then
   exit 0
 fi
 
+VERSION=v0.10.1
+
 TMPDIR=$(mktemp -d)
 cd $TMPDIR
-curl -L -O https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
-unzip exa-linux-x86_64-0.9.0.zip
-mv exa-linux-x86_64 $HOME/.local/bin/exa
+curl -L -O https://github.com/ogham/exa/releases/download/$VERSION/exa-linux-x86_64-$VERSION.zip
+unzip exa-linux-x86_64-$VERSION.zip
+mv bin/exa $HOME/.local/bin/exa
+mv completions/exa.zsh ~/.zsh/completions/_exa
 rm -rf $TMPDIR
