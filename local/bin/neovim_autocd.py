c5245c83118158c0fb96c351fc5d0942b3e8dcd0
Kentaro Wada <kentaro.wada@mujin.co.jp>
2022-09-30 13:36:21 +0900

    Update local/bin/neovim_autocd.py

--- local/bin/neovim_autocd.py
+++ local/bin/neovim_autocd.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # neovim-autocd.py
 import neovim
+import neovim.api
 import os
 
 nvim = neovim.attach('socket', path=os.environ['NVIM'])
-nvim.vars['__autocd_cwd'] = os.getcwd()
-nvim.command('execute "lcd" fnameescape(g:__autocd_cwd)')
-del nvim.vars['__autocd_cwd']
+try:
+    nvim.vars['__autocd_cwd'] = os.getcwd()
+    nvim.command('execute "lcd" fnameescape(g:__autocd_cwd)')
+    del nvim.vars['__autocd_cwd']
+except neovim.api.NvimError:
+    pass
