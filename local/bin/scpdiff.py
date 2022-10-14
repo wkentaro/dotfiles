5cd8ddadaa4f2c7c68d4e400c75ef950749b584c
Kentaro Wada <kentaro.wada@mujin.co.jp>
2022-10-14 12:27:13 +0900

    Update local/bin/scpdiff.py

--- local/bin/scpdiff.py
+++ local/bin/scpdiff.py
@@ -1,14 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
+import os
 import os.path as osp
+import shlex
 import subprocess
+import sys
 import tempfile
+
+
+def run_command(command, verbose=False):
+    if verbose:
+        print(f"+ {command}", file=sys.stderr)
+    subprocess.call(shlex.split(command))
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("file1", help="First file to compare")
 parser.add_argument("file2", help="Second file to compare")
+parser.add_argument("--verbose", "-v", action="store_true", help="Verbose output")
 parser.add_argument("--reverse", action="store_true", help="Reverse the order of the files")
 args = parser.parse_args()
 
@@ -20,10 +30,13 @@ else:
     file2 = args.file2
 
 tmp_dir = tempfile.mkdtemp()
-tmp_file1 = osp.join(tmp_dir, file1)
-tmp_file2 = osp.join(tmp_dir, file2)
+tmp_file1 = osp.join(tmp_dir, file1.replace(':', '_'))
+tmp_file2 = osp.join(tmp_dir, file2.replace(':', '_'))
 
-subprocess.call(["scp", file1, tmp_file1])
-subprocess.call(["scp", file2, tmp_file2])
+os.makedirs(osp.dirname(tmp_file1), exist_ok=True)
+os.makedirs(osp.dirname(tmp_file2), exist_ok=True)
 
-subprocess.call(["diff", tmp_file1, tmp_file2])
+run_command(f"scp -q {file1} {tmp_file1}", verbose=args.verbose)
+run_command(f"scp -q {file2} {tmp_file2}", verbose=args.verbose)
+
+run_command(f"diff {tmp_file1} {tmp_file2}", verbose=args.verbose)
