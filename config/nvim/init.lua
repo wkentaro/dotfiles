1eefb4fbd4dbb7ed477f583c28b2a002249373cb
Kentaro Wada <www.kentaro.wada@gmail.com>
2023-01-24 23:15:11 +0900

    Update config/nvim/init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -214,14 +214,15 @@ require("packer").startup(function()
         set statusline+=%*
         let g:syntastic_always_populate_loc_list = 1
         let g:syntastic_auto_loc_list = 1
-        let g:syntastic_check_on_open = 1
+        let g:syntastic_check_on_open = 0
         let g:syntastic_check_on_wq = 0
-        " let g:syntastic_mode_map = {
-        "     \ "mode": "passive",
-        "     \ "active_filetypes": [],
-        "     \ "passive_filetypes": [] }
+        let g:syntastic_mode_map = {
+            \ "mode": "passive",
+            \ "active_filetypes": [],
+            \ "passive_filetypes": [] }
         let g:syntastic_python_checkers = ['flake8']
         nnoremap <silent> <localleader>f :SyntasticCheck<CR>
+        nnoremap <silent> <localleader>ff :SyntasticReset<CR>
         let g:syntastic_loc_list_height = 5
       ]]
     end,
