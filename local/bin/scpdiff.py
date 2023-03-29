9a1896b9d596485b8f12d01c16963f493eea733b
Kentaro Wada <kentaro.wada@mujin.co.jp>
2023-03-29 16:22:02 +0900

    Update local/bin/scpdiff.py

--- local/bin/scpdiff.py
+++ local/bin/scpdiff.py
@@ -9,25 +9,30 @@ import sys
 import tempfile
 
 
-def run_command(command, verbose=False):
+def run_command(command, verbose=False, ask=False):
+    if ask:
+        answer = input(f"Execute?: {command} [y/N] ")
+        if answer.lower() != "y":
+            print("Aborted", file=sys.stderr)
+            return
+
     if verbose:
         print(f"+ {command}", file=sys.stderr)
     subprocess.call(shlex.split(command))
+
+    if ask:
+        print(f"Executed: {command}", file=sys.stderr)
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("file1", help="First file to compare")
 parser.add_argument("file2", help="Second file to compare")
 parser.add_argument("--verbose", "-v", action="store_true", help="Verbose output")
-parser.add_argument("--reverse", "-r", action="store_true", help="Reverse the order of the files")
+parser.add_argument("--execute", "-e", action="store_true", help="Execute scp command")
 args = parser.parse_args()
 
-if args.reverse:
-    file1 = args.file2
-    file2 = args.file1
-else:
-    file1 = args.file1
-    file2 = args.file2
+file1 = args.file1
+file2 = args.file2
 
 tmp_dir = tempfile.mkdtemp()
 tmp_file1 = osp.join(tmp_dir, file1.replace(':', '_'))
@@ -40,3 +45,6 @@ run_command(f"scp -q {file1} {tmp_file1}", verbose=args.verbose)
 run_command(f"scp -q {file2} {tmp_file2}", verbose=args.verbose)
 
 run_command(f"git --no-pager diff {tmp_file1} {tmp_file2}", verbose=args.verbose)
+
+if args.execute:
+    run_command(f"scp {file1} {file2}", verbose=args.verbose, ask=True)
