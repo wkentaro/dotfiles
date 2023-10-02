cf170099b4e2b7fc1673864fbc97ee8b952e7088
Kentaro Wada <kentaro.wada@mujin.co.jp>
2023-10-02 17:16:04 +0800

    Update config/nvim/lua/base.lua

--- config/nvim/lua/base.lua
+++ config/nvim/lua/base.lua
@@ -56,13 +56,13 @@ vim.cmd [[
 vim.cmd [[
   set grepprg=rg\ --vimgrep\ --no-heading\ --smart-case
   set grepformat=%f:%l:%c:%m,%f:%l:%m
-  nnoremap <leader>e :silent grep<Space>
-  nnoremap <expr> <leader>r ':silent grep! ' . expand('<cword>') . '<CR>' . ':Telescope quickfix<CR>'
-  nnoremap <expr> <leader>e ':silent grep! ' . expand('<cword>') . '<CR>' . ':cw \| wincmd L<CR>'
+  nnoremap <leader>e :silent grep!<Space>
+  nnoremap <expr> <leader>r ':silent grep! ' . expand('<cword>') . '<CR>'
+  " nnoremap <expr> <leader>q ':cw \| wincmd L<CR>'
   nnoremap <silent> [c :cprevious<CR>
   nnoremap <silent> ]c :cnext<CR>
   " au QuickfixCmdPost make,grep,grepadd,vimgrep Telescope quickfix
-  " au QuickfixCmdPost make,grep,grepadd,vimgrep copen | wincmd L
+  au QuickfixCmdPost make,grep,grepadd,vimgrep copen | wincmd L
   " autocmd! FileType qf setl nonumber
 
   aug QFClose
