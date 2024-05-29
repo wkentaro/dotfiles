49750f49f2f5f139bc067a2eab4aabc45851c36d
Kentaro Wada <kentaro.wada@mujin.co.jp>
2024-05-29 18:15:26 -0400

    Install diff-so-fancy correctly

--- scripts/install_diff_so_fancy.sh
+++ scripts/install_diff_so_fancy.sh
@@ -4,5 +4,5 @@ if [ -f $HOME/.local/bin/diff-so-fancy ]; then
   exit 0
 fi
 
-curl -L https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -o $HOME/.local/bin/diff-so-fancy
+curl -L https://github.com/so-fancy/diff-so-fancy/releases/download/v1.4.4/diff-so-fancy -o $HOME/.local/bin/diff-so-fancy
 chmod u+x $HOME/.local/bin/diff-so-fancy
