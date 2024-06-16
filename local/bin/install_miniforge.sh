f4b9914716c917593dbe9e7bf217d365355ebcec
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-06-16 16:22:52 +0900

    Support specifying Miniforge version

--- local/bin/install_miniforge.sh
+++ local/bin/install_miniforge.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-if [ ! $# -eq 1 ]; then
-  echo "Usage: $0 INSTALL_DIR"
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 INSTALL_DIR [MINIFORGE_VERSION]"
   exit 1
 fi
 
 INSTALL_DIR=$1
 INSTALL_DIR=$(cd $INSTALL_DIR && pwd)
+VERSION=${2:-latest}
 
 if [ -e $INSTALL_DIR/.conda ]; then
   echo "Miniforge is already installed: $INSTALL_DIR/.conda"
   exit 0
 fi
 
+echo "Installing Miniforge: $VERSION"
+
 TMPDIR=$(mktemp -d)
 cd $TMPDIR
 
-URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+if [ "${VERSION}" == "latest" ]; then
+    URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+else
+    URL="https://github.com/conda-forge/miniforge/releases/download/${VERSION}/Miniforge3-$(uname)-$(uname -m).sh"
+fi
 
 if which wget &>/dev/null; then
   wget --no-check-certificate -q $URL -O miniforge.sh
