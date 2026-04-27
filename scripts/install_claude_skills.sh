86c7522a50b6b50f0ff5462360c7e2a9931e2d37
Kentaro Wada <kentaro.wada@mujin.co.jp>
2026-04-27 09:57:51 +0900

    remove gstack

--- scripts/install_claude_skills.sh
+++ scripts/install_claude_skills.sh
@@ -4,7 +4,7 @@ SKILLS_DIR="$HOME/.claude/skills"
 
 REPOS=(
   "https://github.com/blader/humanizer.git"
-  "https://github.com/garrytan/gstack.git"
+  # "https://github.com/garrytan/gstack.git"
 )
 
 for repo in "${REPOS[@]}"; do
