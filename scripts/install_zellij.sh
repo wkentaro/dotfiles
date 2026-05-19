f477b3a0e296cfe7b4f72b9805bdb7a5bd8be99b
Kentaro Wada <kentaro.wada@mujin.co.jp>
2026-05-19 15:14:04 +0900

    skip zellij install if already installed

--- scripts/install_zellij.sh
+++ scripts/install_zellij.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+if hash zellij &>/dev/null; then
+  exit 0
+fi
+
+set -e
+
 tmp_path=$(mktemp -d)
 cd $tmp_path
 
 curl -L https://github.com/zellij-org/zellij/releases/download/v0.44.1/zellij-x86_64-unknown-linux-musl.tar.gz -O
 tar zxvf zellij-x86_64-unknown-linux-musl.tar.gz
 
+mkdir -p ~/.local/bin
 mv zellij ~/.local/bin/
