90a1fef0a904d015b72daa7dd1c572eda979a25b
Kentaro Wada <www.kentaro.wada@gmail.com>
2026-08-18 14:56:01 +0900

    use herdr v0.8.0-fork.2

--- scripts/install_herdr.sh
+++ scripts/install_herdr.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-VERSION=v0.8.0-fork.1
-EXPECTED_VERSION="herdr 0.8.0-fork.1"
+VERSION=v0.8.0-fork.2
+EXPECTED_VERSION="herdr 0.8.0-fork.2"
 
 if command -v herdr >/dev/null 2>&1 && [ "$(herdr --version 2>/dev/null)" = "$EXPECTED_VERSION" ]; then
   exit 0
@@ -24,10 +24,10 @@ esac
 asset="herdr-${os}-${arch}"
 
 case "${os}-${arch}" in
-  linux-aarch64) checksum=d0021b4b3112eb2154212eabd9eb24dec405f8eb472c419b799a96548c4d52f2 ;;
-  linux-x86_64)  checksum=b7640af4933faaa5428e0879cea93ca8f13b4828e24141f6166f64babc6bc91a ;;
-  macos-aarch64) checksum=d3611a3bc480851446fcfebb7797a1c19cf41aedfa360424ff1e28cfbd31f3dd ;;
-  macos-x86_64)  checksum=610cb70e96cf69de8838f06226be62fbe658a82e74db35c05df0553e5979127a ;;
+  linux-aarch64) checksum=13d75e7d09bd6d70a23077160dd07710f24ff32c4085b661643e2e1b7bc31f10 ;;
+  linux-x86_64)  checksum=5574b83e0f64552710f57b28e4afb4091a0f0e9f1aa60d80bd875fa32ecad25a ;;
+  macos-aarch64) checksum=4c770566e3d0e2c4fcbc07212fa5f50a51a1062c28969b1e8bd57f83a92e173e ;;
+  macos-x86_64)  checksum=5a6ccc4a2fcd9cd7266db06969a10d4a5f191fdc5bf10651d94070ba446c5f1b ;;
 esac
 
 tmp_path=$(mktemp -d)
